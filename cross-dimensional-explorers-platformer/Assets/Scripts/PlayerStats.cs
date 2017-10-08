b3f4fd38d571d6595048b5f6d909528c905c03d5
Pavel Botezat <pavel.botezat@it.abb.com>
2017-10-08 20:07:55 +0300

    #4 Created start point & death trigger. Improved SpikeBallTrap.prefab

---
+++ cross-dimensional-explorers-platformer/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public GameObject respawnPoint;
+    public LayerMask trap;
+    public int playerLives = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <= -30)
+        {
+            Respawn();
+        }
+	}
+
+    public void Respawn()
+    {
+        if (playerLives <= 0)
+        {
+            transform.position = respawnPoint.transform.position;
+        } else
+        {
+            playerLives -= 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == trap)
+        {
+            Respawn();
+        }
+    }
+}
---
+++ cross-dimensional-explorers-platformer/Assets/Scripts/Trap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+    public PlayerStats player;
+    bool collided = false;
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<PlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collided)
+        {
+            player.Respawn();
+            collided = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        collided = false;
+    }
+}
